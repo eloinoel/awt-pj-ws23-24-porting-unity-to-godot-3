72b729a25f99c72a5f6a0bc17198a225540492c5
Brunololos <70522244+Brunololos@users.noreply.github.com>
2023-12-11 20:28:47 +0100

    Added playable VehicleBody Kart

---
+++ Karting_Godot_V3/KartPlayer.gd
@@ -0,0 +1,13 @@
+extends VehicleBody
+
+var max_rpm = 700
+var max_torque = 200
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("right","left") * 0.4, 5 * delta)
+	var acceleration = Input.get_axis("backward","forward") * 8
+	var rpm = $back_left_wheel.get_rpm()
+	$back_left_wheel.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	rpm = $back_right_wheel.get_rpm()
+	$back_right_wheel.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	
