039551a078c49468f39da5c92a8ad0b30fba6917
Brunololos <70522244+Brunololos@users.noreply.github.com>
2024-01-12 17:29:26 +0100

    Added Background music

--- Karting_Godot_V3/Scripts/AudioManager.cs
+++ Karting_Godot_V3/Scripts/AudioManager.cs
@@ -4,20 +4,21 @@ using Godot;
 
 public class AudioManager
 {
-/*     public AudioStreamPlayer audioMixer;
+    public AudioStreamPlayer audioMixer;
 
     public void EnsureSFXDestruction(AudioStream source)
     {
-        StartCoroutine("DelayedSFXDestruction", source);
+        // StartCoroutine("DelayedSFXDestruction", source);
     }
 
     private IEnumerator DelayedSFXDestruction(AudioStream source)
     {
-        while (source.isPlaying)
+/*         while (source.isPlaying)
         {
             yield return null;
         }
 
-        GameObject.Destroy(source.gameObject);
-    } */
+        GameObject.Destroy(source.gameObject); */
+        return null;
+    }
 }
