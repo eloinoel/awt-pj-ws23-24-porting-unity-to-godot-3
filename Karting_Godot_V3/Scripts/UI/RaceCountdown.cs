a4ba5ae18630222ae5fce892b45d8bad4077b6fc
eloinoel <eloi.sandt.games@gmx.de>
2024-01-17 17:08:28 +0100

    UI Fading functions + RaceCountdown in C#

---
+++ Karting_Godot_V3/Scripts/UI/RaceCountdown.cs
@@ -0,0 +1,93 @@
+using Godot;
+using System;
+
+enum CountdownState {
+    START,
+    THREE,
+    TWO,
+    ONE,
+    GO,
+    FINISHED
+}
+
+public class RaceCountdown : Node
+{
+    AudioStreamSample soundTick;
+    AudioStreamSample soundGo;
+    AudioStreamPlayer audioPlayer;
+
+    CountdownState countdownState = CountdownState.START;
+    float countdownDuration = 1.0f;
+
+    Timer countdownTimer;
+    Node2D objectiveMessage;
+    Label One;
+    Label Two;
+    Label Three;
+    Label Go;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        soundTick = GD.Load<AudioStreamSample>("res://Audio/SFX/StartSound01.wav");
+        soundGo = GD.Load<AudioStreamSample>("res://Audio/SFX/StartSound02.wav");
+
+        if(soundTick == null || soundGo == null)
+        {
+            GD.PrintErr("RaceCountdown: Could not load audio files");
+        }
+
+        audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        countdownTimer = GetNode<Timer>("CountdownTimer");
+        objectiveMessage = GetNode<Node2D>("ObjectiveMessage");
+        One = GetNode<Label>("CenterContainer/One");
+        Two = GetNode<Label>("CenterContainer/Two");
+        Three = GetNode<Label>("CenterContainer/Three");
+        Go = GetNode<Label>("CenterContainer/GO");
+    }
+
+
+    private void _OnTriggerRaceCountdown()
+    {
+        countdownTimer.Start(countdownDuration);
+        audioPlayer.Stream = soundTick;
+        audioPlayer.Play();
+        Three.Visible = true;
+        objectiveMessage.Visible = true;
+        countdownState = CountdownState.THREE;
+    }
+
+    private void _OnCoundownTimerTimeout()
+    {
+        switch(countdownState)
+        {
+            case CountdownState.THREE:
+                countdownTimer.Start(countdownDuration);
+                audioPlayer.Play();
+                Three.Visible = false;
+                Two.Visible = true;
+                countdownState = CountdownState.TWO;
+                break;
+            case CountdownState.TWO:
+                audioPlayer.Play();
+                Two.Visible = false;
+                One.Visible = true;
+                countdownState = CountdownState.ONE;
+                break;
+            case CountdownState.ONE:
+                audioPlayer.Stream = soundGo;
+                audioPlayer.Play();
+                One.Visible = false;
+                Go.Visible = true;
+                countdownState = CountdownState.GO;
+                break;
+            case CountdownState.GO:
+                Go.Visible = false;
+                objectiveMessage.Visible = false;
+                countdownState = CountdownState.FINISHED;
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Karting_Godot_V3/Scripts/Utils/HelperFunctions.cs
+++ Karting_Godot_V3/Scripts/Utils/HelperFunctions.cs
@@ -10,4 +10,41 @@ public class HelperFunctions : Node
     {
         return Time.GetTicksUsec() / 1000000.0f;
     }
+
+    /// <summary>
+    /// fade in the node's alpha
+    /// you can implement the method OnFadeInComplete, if you want to do something after fading
+    /// </summary>
+    public static void FadeIn(CanvasItem node, float duration)
+    {
+        var modulate = node.Modulate;
+        modulate.a = 0f;
+        node.Modulate = modulate;
+        node.Visible = true;
+
+        var tween = new Tween();
+        node.AddChild(tween);
+
+        tween.InterpolateProperty(node, "modulate:a", 0.0f, 1.0f, duration);
+
+        tween.Connect("fade_in_complete", node, "OnFadeInComplete");
+
+        tween.Start();
+    }
+
+    /// <summary>
+    /// fade out the node's alpha
+    /// you can implement the method OnFadeOutComplete, if you want to do something after fading
+    /// </summary>
+    public static void FadeOut(CanvasItem node, float duration)
+    {
+        var tween = new Tween();
+        node.AddChild(tween);
+
+        tween.InterpolateProperty(node, "modulate:a", 1.0f, 0.0f, duration);
+
+        tween.Connect("fade_in_complete", node, "OnFadeOutComplete");
+
+        tween.Start();
+    }
 }
