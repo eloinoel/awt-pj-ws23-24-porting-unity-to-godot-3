a7449ee9fd550b193219027862976ffca5d73232
Brunololos <70522244+Brunololos@users.noreply.github.com>
2024-01-18 14:58:55 +0100

    Further incomplete porting efforts

--- Karting_Godot_V3/Scripts/UI/ObjectiveHUDManager.cs
+++ Karting_Godot_V3/Scripts/UI/ObjectiveHUDManager.cs
@@ -1,69 +1,80 @@
 using System;
 using Godot;
+using Dictionary = System.Collections.Generic.Dictionary<Objective, ObjectiveToast>;
 
-public class ObjectiveHUDManger : Node
+public class ObjectiveHUDManager : Node
 {
 /*     [Export(hintString: "UI panel containing the layoutGroup for displaying objectives")]
-    public UITable objectivePanel;
-    [Export(hintString: "Prefab for the primary objectives")]
-    public PoolObjectDef primaryObjectivePrefab;
-    [Export(hintString: "Prefab for the primary objectives")]
-    public PoolObjectDef secondaryObjectivePrefab;
+	public UITable objectivePanel;
+	[Export(hintString: "Prefab for the primary objectives")]
+	public PoolObjectDef primaryObjectivePrefab;
+	[Export(hintString: "Prefab for the primary objectives")]
+	public PoolObjectDef secondaryObjectivePrefab;*/
+	[Export(hintString: "Prefab for the primary objectives")]
+	public ObjectiveToast primaryObjectivePrefab;
 
-    Dictionary<Objective, ObjectiveToast> m_ObjectivesDictionary; */
+	System.Collections.Generic.Dictionary<Objective, ObjectiveToast> m_ObjectivesDictionary;
 
 	public override void _Ready()
-    {
-        // TODO: m_ObjectivesDictionary = new Dictionary<Objective, ObjectiveToast>();
-    }
+	{
+		m_ObjectivesDictionary = new System.Collections.Generic.Dictionary<Objective, ObjectiveToast>();
+	}
 
-    public void RegisterObjective(Objective objective)
-    {
-        /* TODO: objective.onUpdateObjective += OnUpdateObjective;
+	public void RegisterObjective(Objective objective)
+	{
+		/* TODO: objective.onUpdateObjective += OnUpdateObjective;
 
-        // instanciate the Ui element for the new objective
-        GameObject objectiveUIInstance = objective.isOptional ? secondaryObjectivePrefab.getObject(true, objectivePanel.transform) : primaryObjectivePrefab.getObject(true, objectivePanel.transform);
+		// instanciate the Ui element for the new objective
+		GameObject objectiveUIInstance = objective.isOptional ? secondaryObjectivePrefab.getObject(true, objectivePanel.transform) : primaryObjectivePrefab.getObject(true, objectivePanel.transform);
 
-        if (!objective.isOptional)
-            objectiveUIInstance.transform.SetSiblingIndex(0);
+		if (!objective.isOptional)
+			objectiveUIInstance.transform.SetSiblingIndex(0);
 
-        ObjectiveToast toast = objectiveUIInstance.GetComponent<ObjectiveToast>();
-        DebugUtility.HandleErrorIfNullGetComponent<ObjectiveToast, ObjectiveHUDManger>(toast, this, objectiveUIInstance.gameObject);
+		ObjectiveToast toast = objectiveUIInstance.GetComponent<ObjectiveToast>();
+		DebugUtility.HandleErrorIfNullGetComponent<ObjectiveToast, ObjectiveHUDManger>(toast, this, objectiveUIInstance.gameObject);
 
-        // initialize the element and give it the objective description
-        toast.Initialize(objective.title, objective.description, objective.GetUpdatedCounterAmount(), objective.isOptional, objective.delayVisible);
+		// initialize the element and give it the objective description
+		toast.Initialize(objective.title, objective.description, objective.GetUpdatedCounterAmount(), objective.isOptional, objective.delayVisible);
 
-        m_ObjectivesDictionary.Add(objective, toast);
+		m_ObjectivesDictionary.Add(objective, toast);
 
-        objectivePanel.UpdateTable(toast.gameObject);*/
-    }
+		objectivePanel.UpdateTable(toast.gameObject);*/
+		ObjectiveToast toast = primaryObjectivePrefab;
+		toast.Initialize(objective.title, objective.description, objective.GetUpdatedCounterAmount(), objective.isOptional, objective.delayVisible);
 
-    public void UnregisterObjective(Objective objective)
-    {
-        /* TODO: objective.onUpdateObjective -= OnUpdateObjective;
+		m_ObjectivesDictionary.Add(objective, toast);
 
-        // if the objective if in the list, make it fade out, and remove it from the list
-        if (m_ObjectivesDictionary.TryGetValue(objective, out ObjectiveToast toast))
-            toast.Complete();
-        
-        m_ObjectivesDictionary.Remove(objective); */
-    }
+		//objectivePanel.UpdateTable(toast.gameObject);
+		//((CanvasItem) toast).Visible = true;
+		//toast.Modulate = new Color(1, 1, 1, 0);
+	}
 
-    /*void OnUpdateObjective(UnityActionUpdateObjective updateObjective)
-    {
-        if (m_ObjectivesDictionary.TryGetValue(updateObjective.objective, out ObjectiveToast toast))
-            //&& !string.IsNullOrEmpty(descriptionText))
-        {
-            // set the new updated description for the objective, and forces the content size fitter to be recalculated
-            Canvas.ForceUpdateCanvases();
-            if (!string.IsNullOrEmpty(updateObjective.descriptionText))
-                toast.SetDescriptionText(updateObjective.descriptionText);
+	public void UnregisterObjective(Objective objective)
+	{
+		/* TODO: objective.onUpdateObjective -= OnUpdateObjective;
 
-            if (!string.IsNullOrEmpty(updateObjective.counterText))
-                toast.counterTextContent.text = updateObjective.counterText;
+		// if the objective if in the list, make it fade out, and remove it from the list
+		if (m_ObjectivesDictionary.TryGetValue(objective, out ObjectiveToast toast))
+			toast.Complete();
+		
+		m_ObjectivesDictionary.Remove(objective); */
+	}
 
-            RectTransform toastRectTransform = toast.GetComponent<RectTransform>();
-            if (toastRectTransform != null) UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(toastRectTransform);
-        }
-    } */
+	/*void OnUpdateObjective(UnityActionUpdateObjective updateObjective)
+	{
+		if (m_ObjectivesDictionary.TryGetValue(updateObjective.objective, out ObjectiveToast toast))
+			//&& !string.IsNullOrEmpty(descriptionText))
+		{
+			// set the new updated description for the objective, and forces the content size fitter to be recalculated
+			Canvas.ForceUpdateCanvases();
+			if (!string.IsNullOrEmpty(updateObjective.descriptionText))
+				toast.SetDescriptionText(updateObjective.descriptionText);
+
+			if (!string.IsNullOrEmpty(updateObjective.counterText))
+				toast.counterTextContent.text = updateObjective.counterText;
+
+			RectTransform toastRectTransform = toast.GetComponent<RectTransform>();
+			if (toastRectTransform != null) UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(toastRectTransform);
+		}
+	} */
 }
