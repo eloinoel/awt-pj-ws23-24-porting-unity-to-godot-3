a4231747f5b90a7d0dfd594c55dbae7536b540dd
eloinoel <eloi.sandt.games@gmx.de>
2024-02-06 19:58:30 +0100

    rebased project and fixed laser trail bug

--- Karting_Godot_V3/Scripts/KartSystems/KartAnimation/EnableLaserTrails.gd
+++ Karting_Godot_V3/Scripts/KartSystems/KartAnimation/EnableLaserTrails.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var left_trail: Node
-var right_trail: Node
+var left_trail: Spatial
+var right_trail: Spatial
 
 var trailUptime: float = -1.0
 var trailsActive: bool = false
@@ -11,6 +11,12 @@ func _ready():
     var kart = get_parent()
     left_trail = kart.get_node_or_null("back_left_wheel/Trail3D")
     right_trail = kart.get_node_or_null("back_right_wheel/Trail3D")
+
+    if left_trail != null:
+        left_trail.visible = false
+    if right_trail != null:
+        right_trail.visible = false
+
     pass # Replace with function body.
 
 func _process(delta):
@@ -25,15 +31,22 @@ func enableLaserTrails():
         printerr("Lasertrails are null")
         return
 
-    left_trail.emit = true
-    right_trail.emit = true
+    #left_trail.emit = true
+    #right_trail.emit = true
+    left_trail.visible = true
+    right_trail.visible = true
 
     trailsActive = true
     trailUptime = 3.0
     pass
 
 func disableLaserTrails():
-    left_trail.emit = false
-    right_trail.emit = false
+    #left_trail.emit = false
+    #right_trail.emit = false
+    if left_trail == null or right_trail == null:
+        printerr("Lasertrails are null")
+        return
+    left_trail.visible = false
+    right_trail.visible = false
     trailsActive = false
 
