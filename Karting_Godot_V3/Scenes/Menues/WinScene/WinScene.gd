e4f102d126424293c98549cfb8f98628275bfade
eloinoel <eloi.sandt.games@gmx.de>
2024-02-10 17:11:43 +0100

    Fixed errors with lose and win scenes

--- Karting_Godot_V3/Scenes/Menues/WinScene/WinScene.gd
+++ Karting_Godot_V3/Scenes/Menues/WinScene/WinScene.gd
@@ -1,24 +1,26 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var race_scene_path := "res://Scenes/RaceScene/RaceScene.tscn" setget set_path
+
+func set_path(new_path : String) -> void:
+    race_scene_path = new_path
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-#	var texture = $Viewport.get_texture()
-#	$Screen.texture = texture
+    pass
+#   var texture = $Viewport.get_texture()
+#   $Screen.texture = texture
 
 
 func _on_PlayButton_pressed():
-	print('play')
-	get_tree().change_scene("res://RaceScene.tscn")
-	pass # Replace with function body.
+    get_tree().change_scene(race_scene_path)
+
+func _on_MenuButton_pressed():
+    get_tree().change_scene("res://Scenes/Menues/IntroMenu/MainMenu.tscn")
